a9b59c07d4735642ec7796fcff59b6bfa21d4e8c
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2024-12-21 15:10:43 +0530

    review-schema

---
+++ prisma/review.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function createReview(
+  reviewer: string,
+  review: string,
+  rating: number
+) {
+  try {
+    if (rating < 1 || rating > 5) {
+      throw new Error("Rating must be between 1 and 5");
+    }
+    return prisma.review.create({
+      data: { reviewer, review, rating },
+    });
+  } catch (error) {
+    throw new Error("Error occurred");
+  }
+}
+export async function getReviews() {
+  return prisma.review.findMany();
+}
