07da0ee19a70648c8113edc6649ff874d31e99cc
GulistaKhatun06 <gulista_ug_23@civil.nits.ac.in>
2025-01-31 23:07:58 +0530

    user controller

--- src/controllers/user/create.ts
+++ src/controllers/user/create.ts
@@ -1,5 +1,28 @@
 import * as Interfaces from "../../interfaces";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient(); 
+
 const Create: Interfaces.Controllers.Async = async (req, res) => {
-  //write your post request logic here
+  try {
+    const { name, role, email, firebaseId, posts, tags } = req.body;
+
+    // Create the user in the database
+    const newUser = await prisma.user.create({
+        data: {
+            name,
+            role,
+            email,
+            firebaseId,
+            posts: { connect: posts?.map((postId: string) => ({ id: postId })) }, // Connect existing posts
+            tags: { connect: tags?.map((tagId: string) => ({ id: tagId })) }, // Connect existing tags
+        },
+        include: { posts: true, tags: true }, // Include related data
+    });
+
+    res.status(201).json({ message: "User created successfully", newUser });
+} catch (error) {
+    res.status(500).json({"Error in creating User" });
+}
 };
 export default Create;
--- src/controllers/user/read.ts
+++ src/controllers/user/read.ts
@@ -1,9 +1,45 @@
 import * as Interfaces from "../../interfaces";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 const Read: Interfaces.Controllers.Async = async (req, res) => {
   //write your get request logic here
+  try {
+    const users = await prisma.user.findMany({
+      include:{
+        tags : true,
+        posts : true,
+      }
+    })
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({error:"Something went wrong"})
+  }
 };
 const Self: Interfaces.Controllers.Async = async (req, res) => {
-  //write your get request logic here
+try {
+  const userId = req.user?.id;
+
+  if(!userId){
+    return res.status(401).json({error:"Unauthorized"});
+  }
+    const user = await prisma.user.findUnique({
+      where:{id: userId },
+      include:{
+        tags:true,
+        posts: true,
+      }
+    })
+
+  if(!user){
+    return res.json(404).json({error:"User not found"});
+  }
+  res.json(user);
+ 
+} catch (error) {
+    res.staus(500).json({error:"Something went wrong!"})
+}
+
 };
 
 export { Read, Self };
