259b60620e5ade537ae0430d72f9e0039ad703cf
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-01-31 23:06:52 +0530

    review controller done

--- src/controllers/review/delete.ts
+++ src/controllers/review/delete.ts
@@ -1,5 +1,22 @@
 import * as Interfaces from "../../interfaces";
-const Delete: Interfaces.Controllers.Async = async (req, res) => {
-  //write your delete request logic here
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+const Delete: Interfaces.Controllers.Async = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
+
+  try {
+    await prisma.review.delete({
+      where: { id },
+    });
+
+    res.status(200).json({ message: "Review deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete review" });
+  }
 };
 export default Delete;
--- src/controllers/review/read.ts
+++ src/controllers/review/read.ts
@@ -1,10 +1,44 @@
 import * as Interfaces from "../../interfaces";
-const Read: Interfaces.Controllers.Async = async (req, res) => {
-  //write your get request logic here
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+const Read: Interfaces.Controllers.Async = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
+
+  try {
+    const review = await prisma.review.findUnique({
+      where: { id },
+      include: { Review: true }, //include nested replies
+    });
+    if (!review) {
+      res.status(404).json({ error: "Review not found" });
+    }
+    res.status(200).json(review);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch review" });
+  }
 };
 
-const ReadAll: Interfaces.Controllers.Async = async (req, res) => {
-  //write your get request logic here
+const ReadAll: Interfaces.Controllers.Async = async (
+  req: Request,
+  res: Response
+) => {
+  const { placeId } = req.params;
+
+  try {
+    const reviews = await prisma.review.findMany({
+      where: { placeId },
+      include: { Review: true }, // include replies
+    });
+    res.status(200).json({ reviews });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch reviews" });
+  }
 };
 
 export { Read, ReadAll };
