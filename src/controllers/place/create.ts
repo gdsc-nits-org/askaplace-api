34f5a17b75dab0903cf5e6e0da8717ec4025773b
Manashgoswamii <goswamimanash02@gmail.com>
2025-02-01 20:11:05 +0530

    Place controller done

--- src/controllers/place/create.ts
+++ src/controllers/place/create.ts
@@ -1,5 +1,55 @@
 import * as Interfaces from "../../interfaces";
-const Create: Interfaces.Controllers.Async = async (req, res) => {
-  //write your post request logic here
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+const Create: Interfaces.Controllers.Async = async (
+  req: Request,
+  res: Response
+) => {
+  const {
+    photos,
+    place,
+    description,
+    location,
+    longitiude,
+    latitude,
+    mode_of_travel,
+    blog,
+    upvotes,
+    downvotes,
+    expense,
+    placeId,
+    authorId,
+    tags,
+  } = req.body;
+
+  try {
+    const newPlace = await prisma.place.create({
+      data: {
+        photos,
+        place,
+        description,
+        location,
+        longitiude,
+        latitude,
+        mode_of_travel,
+        blog,
+        upvotes,
+        downvotes,
+        expense,
+        placeId,
+        authorId,
+        tags: {
+          connect: tags?.map((tagId: string) => ({ id: tagId })) || [],
+        },
+      },
+    });
+    res.status(201).json(newPlace);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create place!" });
+  }
 };
+
 export default Create;
