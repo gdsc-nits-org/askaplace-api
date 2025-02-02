219afdefd1f8f2ed85936e5077fc6caa6b34bb62
Akshita Shahi <akshitas0806@gmail.com>
2025-02-02 17:20:53 +0530

    stayController

--- src/controllers/stay/delete.ts
+++ src/controllers/stay/delete.ts
@@ -1,5 +1,20 @@
 import * as Interfaces from "../../interfaces";
-const Delete: Interfaces.Controllers.Async = async (req, res) => {
-  //write your delete request logic here
+import {Request,Response} from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma=new PrismaClient();
+
+const Delete: Interfaces.Controllers.Async = async (req:Request, res:Response) => {
+    try{
+      const {id}=req.params;
+
+      await prisma.stay.delete({
+        where:{id},
+      });
+
+      res.json({message:"Stay deleted successfully"});
+    }catch(error){
+      res.status(500).json({error:"Failed to delete stay"});
+    }
 };
 export default Delete;
--- src/controllers/stay/update.ts
+++ src/controllers/stay/update.ts
@@ -1,5 +1,22 @@
 import * as Interfaces from "../../interfaces";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient(); 
 const Update: Interfaces.Controllers.Async = async (req, res) => {
-  //write your patch request logic here
+  //write your patch request logic here\
+  try {
+    const{id} = req.params;
+    const{name,type , website, location, landmark, contact, manager, place ,placeId} = req.body;
+    const existingStay = await prisma.stay.findUnique({where:{id}});
+    if(!existingStay){
+      return res.status(404).json({message:"Stay not found"});
+    }
+      const updatedStay = await prisma.stay.update({where:{id}, data:{name, type ,website,location, landmark,contact, manager,place, placeId}});
+      res.json(updatedStay);
+    
+  } catch (error) {
+    
+  res.status(500).json({message:"Error updating stay",error});
+  }
 };
 export default Update;
