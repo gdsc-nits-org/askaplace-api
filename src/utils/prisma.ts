63a54544c79cf977a5dc5b573af3883e1255e81a
Manashgoswamii <goswamimanash02@gmail.com>
2025-06-02 12:08:23 +0530

    route fixes

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+async function testConnection() {
+  try {
+    // Try a simple query to check DB connection
+    await prisma.$connect();
+    console.log("✅ MongoDB connected successfully via Prisma.");
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB via Prisma:", error);
+  }
+}
+
+testConnection();
 
 export default prisma;
